2cd088bc11ad28ecaf84b12ffaaf1db6202baf1d
Viniciusadm <viniciusam.22@gmail.com>
2021-09-16 22:22:09 -0300

    Adiçao do projeto salario

---
+++ salario/Funcionário.cs
@@ -0,0 +1,22 @@
+namespace salario {
+    public class Funcionário {
+        public int ID { get; private set; }
+        public string Nome { get; private set; }
+        public double Salário { get; private set; }
+
+        public Funcionário(int id, string nome, double salário) {
+            this.ID = id;
+            this.Nome = nome;
+            this.Salário = salário;
+        }
+
+        public void AumentarSalário(double porcentagem) {
+            double valorPorcentagem = (porcentagem * this.Salário) / 100;
+            this.Salário += valorPorcentagem;
+        }
+
+        public override string ToString() {
+            return $"ID: {this.ID}, Nome: {this.Nome}, Salário: {this.Salário}";
+        }
+    }
+}
---
+++ salario/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace salario {
+    class Program {
+        static void Main(string[] args) {
+            List<Funcionário> funcionários = new List<Funcionário>();
+
+            Console.Write("Quantos funcionários serão registrados? ");
+            int quantidadeDeFuncionários = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= quantidadeDeFuncionários; i++) {
+                Console.WriteLine($"Funcionário {i}");
+                Console.Write("ID: ");
+                int id = int.Parse(Console.ReadLine());
+
+                Console.Write("Nome: ");
+                string nome = Console.ReadLine();
+
+                Console.Write("Salário: ");
+                double salário = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                funcionários.Add(new Funcionário(id, nome, salário));
+            }
+
+            Console.Write("Digite o ID do funcionário que receberá o aumento: ");
+            int idDoFuncionário = int.Parse(Console.ReadLine());
+
+            if (funcionários.Exists(funcionário => funcionário.ID == idDoFuncionário)) {
+                Console.Write("Porcentagem do aumento: ");
+                double aumento = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                
+                int indiceDoFuncionário = funcionários.FindIndex(funcionário => funcionário.ID == idDoFuncionário);
+                funcionários[indiceDoFuncionário].AumentarSalário(aumento);
+
+                Console.WriteLine("\nDados dos funcionário: ");
+                foreach (Funcionário funcionário in funcionários) {
+                    Console.WriteLine(funcionário);
+                }
+            } else {
+                Console.WriteLine("Funcionário inexistente");
+            }
+        }
+    }
+}
